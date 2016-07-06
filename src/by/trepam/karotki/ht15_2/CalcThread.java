4e582047a7fb7b3fb729c22113d7a80e473fb7df
Viachaslau Korotki <Viachaslau Korotki>
2016-07-07 00:56:39 +0300

    commit

---
+++ src/by/trepam/karotki/ht15_2/CalcThread.java
@@ -0,0 +1,5 @@
+package by.trepam.karotki.ht15_2;
+
+public class CalcThread implements Runnable {
+
+}
---
+++ src/by/trepam/karotki/ht15_2/CalculatePerformer.java
@@ -0,0 +1,6 @@
+package by.trepam.karotki.ht15_2;
+
+public class CalculatePerformer {
+
+ 
+}
